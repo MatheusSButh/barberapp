2c6977031d465b16a03a289f063eefff90744fc8
Matheus <matheusdsouzabuth@gmail.com>
2025-06-10 10:36:25 -0300

    UserService

---
+++ src/main/java/com/buthdev/demo/services/UserService.java
@@ -0,0 +1,36 @@
+package com.buthdev.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.buthdev.demo.dtos.request.UserRequestDTO;
+import com.buthdev.demo.model.User;
+import com.buthdev.demo.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User createUser(UserRequestDTO userDTO) {
+		User user = convertToUser(userDTO);
+		
+		return userRepository.save(user);
+	}
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	
+	private User convertToUser(UserRequestDTO userDTO) {
+		User user = new User();
+		BeanUtils.copyProperties(userDTO, user);
+		
+		return user;
+	}
+}
