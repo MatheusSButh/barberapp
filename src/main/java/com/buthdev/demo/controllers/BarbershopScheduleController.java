0206657162a2d81915694afabfd1bb746e1d6719
Matheus <matheusdsouzabuth@gmail.com>
2025-06-16 17:12:32 -0300

    Refactor - reservedTimeController to BarbershopScheduleController, freeTimeController to UserScheduleController

--- src/main/java/com/buthdev/demo/controllers/BarbershopScheduleController.java
+++ src/main/java/com/buthdev/demo/controllers/BarbershopScheduleController.java
@@ -8,33 +8,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.buthdev.demo.dtos.request.ReservedTimeRequestDTO;
 import com.buthdev.demo.dtos.response.ReservedTimeResponseDTO;
-import com.buthdev.demo.model.ReservedTime;
 import com.buthdev.demo.services.ScheduleService;
 
 @RestController
 @RequestMapping(value = "schedule")
-public class ReservedTimeController {
+public class BarbershopScheduleController {
 
 	@Autowired
 	ScheduleService scheduleService;
 	
-	@PostMapping
-	public ResponseEntity<ReservedTime> createReservedTime(@RequestBody ReservedTimeRequestDTO reservedTimeDTO) {
-		scheduleService.createReservedTime(reservedTimeDTO);
-		
-		return ResponseEntity.status(HttpStatus.CREATED).build();
-	}
-	
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+	public ResponseEntity<Void> deleteReservedTime(@PathVariable Long id) {
 		scheduleService.deleteReservedTime(id);
 		
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
--- src/main/java/com/buthdev/demo/controllers/UserScheduleController.java
+++ src/main/java/com/buthdev/demo/controllers/UserScheduleController.java
@@ -3,21 +3,33 @@ package com.buthdev.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.buthdev.demo.dtos.request.ReservedTimeRequestDTO;
 import com.buthdev.demo.dtos.response.FreeTimesResponseDTO;
+import com.buthdev.demo.model.ReservedTime;
 import com.buthdev.demo.services.ScheduleService;
 
 @RestController
 @RequestMapping(value = "userSchedule")
-public class FreeTimesController {
+public class UserScheduleController {
 
 	@Autowired
 	private ScheduleService scheduleService;
+	
+	@PostMapping
+	public ResponseEntity<ReservedTime> createReservedTime(@RequestBody ReservedTimeRequestDTO reservedTimeDTO) {
+		scheduleService.createReservedTime(reservedTimeDTO);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
 	
 	@GetMapping(value = "/times")
 	public ResponseEntity<List<FreeTimesResponseDTO>> findAllFreeTimes(@RequestParam String date) {
