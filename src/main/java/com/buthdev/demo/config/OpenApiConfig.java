a52afa7b294c4b09b67ca711cf373b33b9e6ca75
Matheus <matheusdsouzabuth@gmail.com>
2025-06-21 17:51:06 -0300

    OpenApiConfig - Swagger

---
+++ src/main/java/com/buthdev/demo/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.buthdev.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@Configuration
+@OpenAPIDefinition(
+		info = @Info(title = "Barber API", version = "v1", description = "API para agendamento de serviços de uma barbearia"),
+		security = @SecurityRequirement(name = "bearerAuth")
+		)
+
+@SecurityScheme(
+		name = "bearerAuth",
+		type = SecuritySchemeType.HTTP,
+		scheme = "bearer",
+		bearerFormat = "JWT"
+		)
+
+public class OpenApiConfig {
+ 
+}
+
